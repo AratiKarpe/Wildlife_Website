e90ed2ce38d85fa111378fe5a2a6e7e36d5d3ea7
Arati Karpe <karpearati76@gmail.com>
2025-01-14 12:53:56 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php session_start(); ?>
+<?php include('dbcon.php'); ?>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style1.css">
+</head>
+<body>
+<div class="form-wrapper">
+  
+  <form action="#" method="post">
+    <h3>Login here</h3>
+	
+    <div class="form-item">
+		<input type="text" name="username" required="required" placeholder="Username" autofocus required></input>
+    </div>
+    
+    <div class="form-item">
+		<input type="password" name="password" required="required" placeholder="Password" required></input>
+    </div>
+    
+    <div class="button-panel">
+		<input type="submit" class="button" title="Log In" name="login" value="Login"></input>
+    </div>
+  </form>
+  <?php
+	if (isset($_POST['login']))
+		{
+			$username = mysqli_real_escape_string($con, $_POST['username']);
+			$password = mysqli_real_escape_string($con, $_POST['password']);
+			
+			$query 		= mysqli_query($con, "SELECT * FROM users WHERE  password='$password' and username='$username'");
+			$row		= mysqli_fetch_array($query);
+			$num_row 	= mysqli_num_rows($query);
+			
+			if ($num_row > 0) 
+				{			
+					$_SESSION['user_id']=$row['user_id'];
+					header('location:index.html');
+					
+				}
+			else
+				{
+					// echo 'Invalid Username and Password Combination';
+					echo '<script>alert("Invalid Username and Password Combination")</script>';
+				}
+		}
+  ?>
+  <!-- <div class="reminder">
+    <p>Not a member? <a href="#">Please Sign up now</a></p>
+    <p><a href="#">Forgot password?</a></p>
+  </div> -->
+  
+</div>
+ 
+</body>
+</html>
